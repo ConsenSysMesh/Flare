a65c670dddfcfd310219beb07b3744eb5dd1b319
Developer <dev@grid.home>
2015-07-10 17:14:35 -0400

    changed a little bit off CSS

--- node_server/views/connections.jsx
+++ node_server/views/connections.jsx
@@ -6,11 +6,13 @@ var Connections = React.createClass({
 	displayName: "Connections",
     render: function(){
     	return(
+			<div className='outline'>
 			<div className='row'>
       			<div className='container connections'>
       			  <h3>Connections</h3>
       			</div>
     		</div>
+			</div>
 		)
     }
 })
--- node_server/views/settings.jsx
+++ node_server/views/settings.jsx
@@ -6,11 +6,13 @@ var Settings = React.createClass({
 	displayName: "Settings",
     render: function(){
     	return(
+			<div className='outline'>
 			<div className='row'>
       			<div className='container settings'>
       			  <h3>Settings</h3>
       			</div>
     		</div>
+			</div>
 		)
     }
 })
