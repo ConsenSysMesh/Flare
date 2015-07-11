5a09d1f3bb5a818ff80f862c425bdf6687b35155
Firescar96 <nchinda2@gmail.com>
2015-07-11 04:00:12 -0400

    file structure and broken code

--- go_server/websockets.go
+++ go_server/websockets.go
@@ -6,11 +6,9 @@ package main
 
 import (
 	"flag"
-	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
-	"os"
-	"strconv"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -30,6 +28,12 @@ const (
 	filePeriod = 10 * time.Second
 )
 
+type websocketParams struct {
+	socket *websocket.Conn
+}
+
+var ws = websocketParams{}
+
 var (
 	addr     = flag.String("addr", ":8080", "http service address")
 	filename string
@@ -39,77 +43,51 @@ var (
 	}
 )
 
-func readFileIfModified(lastMod time.Time) ([]byte, time.Time, error) {
-	fi, err := os.Stat(filename)
-	if err != nil {
-		return nil, lastMod, err
-	}
-	if !fi.ModTime().After(lastMod) {
-		return nil, lastMod, nil
-	}
-	p, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, fi.ModTime(), err
-	}
-	return p, fi.ModTime(), nil
-}
-
-func reader(ws *websocket.Conn) {
-	defer ws.Close()
-	ws.SetReadLimit(512)
-	ws.SetReadDeadline(time.Now().Add(pongWait))
-	ws.SetPongHandler(func(string) error { ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+func reader() {
+	defer ws.socket.Close()
+	ws.socket.SetReadLimit(512)
+	ws.socket.SetReadDeadline(time.Now().Add(pongWait))
+	ws.socket.SetPongHandler(func(string) error { ws.socket.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 	for {
-		_, _, err := ws.ReadMessage()
+		_, _, err := ws.socket.ReadMessage()
 		if err != nil {
 			break
 		}
 	}
 }
 
-func writer(ws *websocket.Conn, lastMod time.Time) {
-	lastError := ""
-	pingTicker := time.NewTicker(pingPeriod)
-	fileTicker := time.NewTicker(filePeriod)
-	defer func() {
-		pingTicker.Stop()
-		fileTicker.Stop()
-		ws.Close()
-	}()
-	for {
-		select {
-		case <-fileTicker.C:
-			var p []byte
-			var err error
-
-			p, lastMod, err = readFileIfModified(lastMod)
-
-			if err != nil {
-				if s := err.Error(); s != lastError {
-					lastError = s
-					p = []byte(lastError)
-				}
-			} else {
-				lastError = ""
-			}
-
-			if p != nil {
-				ws.SetWriteDeadline(time.Now().Add(writeWait))
-				if err := ws.WriteMessage(websocket.TextMessage, p); err != nil {
-					return
-				}
-			}
-		case <-pingTicker.C:
-			ws.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := ws.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
-				return
-			}
-		}
+func writer(message []byte) {
+	ws.socket.SetWriteDeadline(time.Now().Add(writeWait))
+	if err := ws.socket.WriteMessage(websocket.TextMessage, message); err != nil {
+		log.Fatal(err)
 	}
 }
 
+func pingWriter() {
+	pingTicker := time.NewTicker(pingPeriod)
+
+	defer func() {
+		pingTicker.Stop()
+		ws.socket.Close()
+	}()
+	go func() {
+		for {
+			select {
+			case <-pingTicker.C:
+				ws.socket.SetWriteDeadline(time.Now().Add(writeWait))
+				if err := ws.socket.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+	}()
+}
+
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
+	conn, err := net.Dial("tcp", "localhost:38477")
+	socket, err := websocket.NewClient(conn, "/", nil)
+	ws.socket = socket
+
 	if err != nil {
 		if _, ok := err.(websocket.HandshakeError); !ok {
 			log.Println(err)
@@ -117,19 +95,13 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var lastMod time.Time
-	if n, err := strconv.ParseInt(r.FormValue("lastMod"), 16, 64); err != nil {
-		lastMod = time.Unix(0, n)
-	}
-
-	go writer(ws, lastMod)
-	reader(ws)
+	log.Println("adad")
+	pingWriter()
+	reader()
 }
 
-func getSparkLog(file string) {
-	filename = file
-
-	http.HandleFunc("/ws", serveWs)
+func initWebsockets() {
+	http.HandleFunc("/", serveWs)
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal(err)
 	}
